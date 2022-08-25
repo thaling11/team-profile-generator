f6fcd65e9cc647b10a66ff7f4d1bb005ee5224ff
thaling <thaling@verizon.net>
2022-08-24 20:22:44 -0600

    index.js set up

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,48 @@
 const inquirer = require("inquirer");
-const generateManager = require("./lib/manager");
-const generateEngineer = require("./lib/engineer");
-const generateIntern = require("./lib/intern");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
 const fs = require("fs");
+let html = `<!DOCTYPE>`
 
-inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "What is your name?",
-            name: "name",
-        }
-    ])
+function makeEmployee() {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What type of team member are you adding?",
+      name: "employeeType",
+      choices: ["Manager", "Engineer", "Intern", "All done adding"],
+    },
+  ]).then((data) => {
+    //   if()
+  })
+
+  //manager
+  function manager() {
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the employee's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is the employee's ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the employee's email?",
+        name: "email",
+      },
+    ]).then((data) => {
+        const manager = new Manager()
+        const managerinput = `<div>${manager.name}</div>`
+        html += managerinput
+        makeEmployee();
+    });
+  }
+}
+
+const endhtml = `</div></body></html>`
+//
--- Develop/lib/employee.js
+++ Develop/lib/employee.js
@@ -1,1 +1,21 @@
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
 
+getName() {
+    return this.name;
+}
+
+// getId()
+
+// getEmail()
+
+getRole() {
+    return "Employee"
+}
+}
+
+module.exports = Employee
