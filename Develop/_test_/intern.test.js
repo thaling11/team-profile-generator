4b74c49c99a50829815be607485f228b1aced6fd
thaling <thaling@verizon.net>
2022-08-26 11:11:04 -0600

    intern tests and functions

--- Develop/_test_/intern.test.js
+++ Develop/_test_/intern.test.js
@@ -1,7 +1,23 @@
 const Intern = require('../lib/intern');
 
 // school
+it("should set the intern's school", () => {
+    const schoolInput = "DU";
+    const newEmployee = new Intern("Sally", 1234, "employee@email.com", schoolInput);
+    expect(newEmployee.school).toBe(schoolInput);
+});
 
 // getSchool()
+it("should return github with getGithub()", () => {
+    const schoolInput = "DU";
+    const newEmployee = new Intern("Sally", 1234, "employee@gmail.com", schoolInput);
+    expect(newEmployee.getSchool()).toBe(schoolInput);
+});
 
 // getRole()—overridden to return 'Intern'
+it("should return Intern input with getRole()", () => {
+    const internInput = "Intern";
+    const newEmployee = new Intern("Sally", 1234, "employee@gmail.com", "DU");
+    expect(newEmployee.getRole()).toBe(internInput);
+})
+
