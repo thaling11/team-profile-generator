0e424970f759c49cb79db23fc1d1e083c1e62d6a
thaling <thaling@verizon.net>
2022-08-26 11:05:12 -0600

    engineer test and functions

--- Develop/_test_/engineer.test.js
+++ Develop/_test_/engineer.test.js
@@ -1,8 +1,22 @@
 const Engineer = require('../lib/engineer')
 
-
 // github—GitHub username
+it("should set a github username", () => {
+    const githubUser = "Sally12";
+    const newEmployee = new Engineer("Sally", 1234, "employee@email.com", githubUser);
+    expect(newEmployee.github).toBe(githubUser);
+});
 
 // getGithub()
+it("should return github with getGithub()", () => {
+    const githubUser = "Sally12";
+    const newEmployee = new Engineer("Sally", 1234, "employee@gmail.com", githubUser);
+    expect(newEmployee.getGithub()).toBe(githubUser);
+});
 
 // getRole()—overridden to return 'Engineer'
+it("should return Engineer input with getRole()", () => {
+    const engineerInput = "Engineer";
+    const newEmployee = new Engineer("Sally", 1234, "employee@gmail.com", "Sally12");
+    expect(newEmployee.getRole()).toBe(engineerInput);
+})
