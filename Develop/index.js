5bdf36cb70f9f8f794e42ecab64dd09e81e4e273
thaling <thaling@verizon.net>
2022-08-27 15:36:58 -0600

    working prompts initial and manager

--- Develop/index.js
+++ Develop/index.js
@@ -3,49 +3,67 @@ const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const fs = require("fs");
-let html = `<!DOCTYPE>`
+let html = `<!DOCTYPE>`;
 
 function makeEmployee() {
-  inquirer.prompt([
-    {
-      type: "list",
-      message: "What type of team member are you adding?",
-      name: "employeeType",
-      choices: ["Manager", "Engineer", "Intern", "All done adding"],
-    },
-  ]).then((data) => {
-      if(employeeType === "Manager"){
-        manager();
-      } 
-  })
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What type of team member are you adding?",
+        name: "employeeType",
+        choices: ["Manager", "Engineer", "Intern", "All done adding"],
+      },
+    ])
+    .then((data) => {
+      switch (data.employeeType) {
+        case "Manager":
+          manager();
+          break;
+
+        // case "Engineer":
+        //   engineer();
+        //   break;
+
+        // case "Intern":
+        //   engineer();
+        //   break;
+
+        // case "All done adding":
+        //   createHTML();
+        //   break;
+      }
+    });
 
   //manager
   function manager() {
-    inquirer.prompt([
-      {
-        type: "input",
-        message: "What is the employee's name?",
-        name: "name",
-      },
-      {
-        type: "input",
-        message: "What is the employee's ID?",
-        name: "id",
-      },
-      {
-        type: "input",
-        message: "What is the employee's email?",
-        name: "email",
-      },
-    ]).then((data) => {
-        const manager = new Manager()
-        const managerinput = `<div>${manager.name}</div>`
-        html += managerinput
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the employee's name?",
+          name: "name",
+        },
+        {
+          type: "input",
+          message: "What is the employee's ID?",
+          name: "id",
+        },
+        {
+          type: "input",
+          message: "What is the employee's email?",
+          name: "email",
+        },
+      ])
+      .then((data) => {
+        const manager = new Manager();
+        const managerinput = `<div>${manager.name}</div>`;
+        html += managerinput;
         makeEmployee();
-    });
+      });
   }
 }
 
-const endhtml = `</div></body></html>`
+const endhtml = `</div></body></html>`;
 //
 makeEmployee();
