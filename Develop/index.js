577352b552e5d4db28c911732d4d43a196fbd9f3
thaling <thaling@verizon.net>
2022-08-31 09:02:30 -0600

    final CSS edits

--- Develop/index.js
+++ Develop/index.js
@@ -155,25 +155,43 @@ function createHTML() {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
       <link href="https://fonts.googleapis.com/css2?family=Sarabun&display=swap" rel="stylesheet">  
       <style>
-       
         body {
           background-color:	#C7DDCC; 
           color: #16123F;
           font-family: 'Sarabun', sans-serif;
         } 
         .container {
-          display:flex;
+          display: inline-flex;
           justify-content: space-evenly;
           flex-wrap: wrap;
         } 
         .card {
           background-color: #16123F; 
-          margin: 20px;
+          margin: 50px 20px 20px 20px;
           color: white; 
-          display: flex; 
           padding: 10px;
-          width: 30%;
+          width: 200px;
           border: 5px solid #40E0D0;
+        } 
+        h1 {
+          text-align: center;
+          background-color: rgb(236, 236, 170);
+          padding: 10px;
+          margin: 0;
+        }
+        .card-header {
+          background-color: #ABD699;
+          color: #16123F;
+          margin: -10px;
+          padding: 25px;
+          font-weight: 700;
+          font-size: 25px;
+        }
+        h3 {
+          font-weight: 300;
+        }
+        a {
+          color: #40E0D0;
         }
       </style>
     </head>
@@ -183,13 +201,13 @@ function createHTML() {
   html += startHTML;
 
   for (let index = 0; index < teamMembers.length; index++) {
-    let teamCard = `<div class="container"><div class="card"><div class="card-header">${teamMembers[index].role}</div><div class="card-body"><h2 class="card-title">${teamMembers[index].name}</h2><h3 class="id">ID: ${teamMembers[index].id}</h3><h3 class="email">Email: ${teamMembers[index].email}</h3>`;
+    let teamCard = `<div class="container"><div class="card"><div class="card-header">${teamMembers[index].role}</div><div class="card-body"><h2 class="card-title">${teamMembers[index].name}</h2><h3 class="id">ID: ${teamMembers[index].id}</h3><h3>Email: <a href="mailto:${teamMembers[index].email}">${teamMembers[index].email}</a></h3>`;
 
     if (teamMembers[index].officeNumber) {
-      teamCard += `<h3>Office Number: ${teamMembers[index].officeNumber}</h3>`;
+      teamCard += `<h3>Phone: ${teamMembers[index].officeNumber}</h3>`;
     }
     if (teamMembers[index].github) {
-      teamCard += `<h3>Github: ${teamMembers[index].github}</h3>`;
+      teamCard += `<h3>Github: <a href="https://github.com/${teamMembers[index].github}" target="_blank">${teamMembers[index].github}</a></h3>`;
     }
     if (teamMembers[index].school) {
       teamCard += `<h3>School: ${teamMembers[index].school}</h3>`;
